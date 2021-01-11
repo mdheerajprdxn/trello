8f1049207d1ae03293b66908809e19b51f37b099
mdheerajprdxn <dheerajm@prdxn.com>
2021-01-11 19:27:07 +0530

    [js] added drag drop functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,22 @@
 
 */
 
+let tasks = document.querySelectorAll(".task");
+let taskList = document.querySelectorAll(".task-list");
 
+tasks.forEach((task) => {
+  task.addEventListener("dragstart", () => {
+    task.classList.add("dragging");
+  });
+  task.addEventListener("dragend", () => {
+    task.classList.remove("dragging");
+  });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+taskList.forEach((list) => {
+  list.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    let task = document.querySelector(".dragging");
+    list.append(task);
+  });
+});
