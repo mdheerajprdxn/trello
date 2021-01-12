b2a4f6c83203da746d9882b9c63a8e402510cd38
mdheerajprdxn <dheerajm@prdxn.com>
2021-01-12 10:29:52 +0530

    [js] testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,15 @@
 
 let tasks = document.querySelectorAll(".task");
 let taskList = document.querySelectorAll(".task-list");
+let addtasks = document.querySelectorAll(".add-task-form");
+
+addtasks.forEach((addTask) => {
+  addTask.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let task = e.target.querySelector("input").value;
+    console.log(task);
+  });
+});
 
 tasks.forEach((task) => {
   task.addEventListener("dragstart", () => {
